ad8db75d11549e7deb3cb955696944e790be93fe
Drew Kabala <kabalzo@outlook.com>
2023-08-24 21:45:57 -0500

    Added logic to PlayHand, changed RollAllDice
    The PlayHand method is now used within Playgame to allow the user to
    keep dice or reroll them. Rewrote RollAllDice with for loop. Added
    TODOs for things that need to be implemented. Currently no way to
    control the scorecard entires. At the end of each turn the total is
    added to the running total. Need to move the die into an array for
    easier referencing. Need to implement a scorcard system.

--- Yahtzee Class.cs
+++ Yahtzee Class.cs
@@ -1,8 +1,14 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
 
 class Yahtzee {
-
+    Tuple<string, bool, int>[] ScoreBoard = 
+    {
+        Tuple.Create("yahtzee", false, 50),
+        Tuple.Create("large", false, 40),
+        Tuple.Create("small", false, 30),
+    };
     private Random roll1 = new();
     private Random roll2 = new();
     private Random roll3 = new();
@@ -14,11 +20,14 @@ class Yahtzee {
     private int handScore = 0; 
     private int turnCounter = 1;
 
+    //TODO: Change this from separate ints to 5 int elements in an array
     private int die1;
     private int die2;  
     private int die3;  
     private int die4; 
-    private int die5; 
+    private int die5;
+
+    //private readonly int[] ListOfDice = new int[] { die1, die2, die3, die4, die5 };
 
     private readonly string theRules = "\nRoll 5 dice on your first turn\nKeep 0-5 and roll up to twice more with the rest\nFill entries on the scorecard each turn with the result of the rolls\n\nUPPER SECTION\n(Aces) Count and add only aces\n(Twos) Count and add only twos\n(Threes) Count and add only threes\n(Fours) Count and add only fours\n(Fives) Count and add only fives\n(Sixes) Count and add only sixes\nIf total score of upper is 63 or greater, bonus 35 points\n\nLOWER SECTION\n(3 of a kind) Add total of all dice\n(4 of a kind) Add total of all dice\n(Full House) Two of one kind, three of another. Score 25\n(Small straight) Sequence of 4. Score 30\n(Large straight) Sequence of 5. Score 40\n(Yahtzee) 5 of a kind. Score 50\n(Chance) Add total of all dice\n(Yahtzee bonus) 100 bonus points for each additional Yahtzee after the first\n";
 
@@ -51,11 +60,7 @@ class Yahtzee {
 
     //Display the values of all 5 die at any given moment
     private void DisplayCurrentDiceValues() {
-        Console.WriteLine("Die 1: " + die1);
-        Console.WriteLine("Die 2: " + die2);
-        Console.WriteLine("Die 3: " + die3);
-        Console.WriteLine("Die 4: " + die4);
-        Console.WriteLine("Die 5: " + die5 + "\n");
+        Console.WriteLine("Die 1: " + die1 + " | Die 2: " + die2 + " | Die 3: " + die3 + " | Die 4: " + die4 + " | Die 5: " + die5);
     }
     
     //Ask user if they want to see the rules or proceed to the game
@@ -100,7 +105,49 @@ class Yahtzee {
     //This will be used for holding dice and adding up the score for a hand
     //instead of just rerolling all the dice as it is right now
     private void PlayHand() {
-        int handRoll = 0;
+        RollAllDice();
+        string keepDiceOrRollAgain;
+        string endTurnEarly;
+        //Iterate three times at most per hand
+        for (int currentRollCount = 0; currentRollCount <= 1; currentRollCount++) {
+            //Ask user if they want to keep each die each roll.
+            //Definetely a more elegant way to do this but as a proof of concept
+            Console.WriteLine($"This is roll {currentRollCount +1}");
+             DisplayCurrentDiceValues();
+            for (int currentDie = 1; currentDie <=5; currentDie++) {
+                while (true) {
+                    //DisplayCurrentDiceValues();
+                    Console.WriteLine($"Do you want to keep dice {currentDie} ? [y] [n]");
+                    keepDiceOrRollAgain = Console.ReadLine();
+                        if (keepDiceOrRollAgain == "n") {
+                            RollSingleDie(currentDie);
+                            break;
+                        }
+                        else if (keepDiceOrRollAgain == "y") {
+                            break;
+                        }
+                        else {
+                            Console.WriteLine("Invalid entry\n");
+                        }
+                }
+            }
+            //Give user the option to score the hand early and not have to roll three times if the dice are what they want
+            //DisplayCurrentDiceValues();
+            Console.WriteLine("Would you like to score this roll? [y] [n]\n");
+            endTurnEarly = Console.ReadLine();
+
+            //For right now just add the total of the dice to the total score
+            //TODO: Implement a scoring procedure to add the score to the right portion of the scorecard and account for points
+            if (endTurnEarly == "y") {
+                totalScore += die1 + die2 + die3 + die4 + die5;
+                break;
+            }
+        }
+        //TODO: This is where you would have to select the scorcard slot of your hand having rolled three times
+        Console.Write("Results of the final throw are: ");
+        DisplayCurrentDiceValues();
+        totalScore += die1 + die2 + die3 + die4 + die5;
+        Console.WriteLine($"Current total score is: {totalScore}\n");
     }
 
     //Main method to invoke the game. Only call this from another file
@@ -128,12 +175,20 @@ class Yahtzee {
                     continue;
                 //Enter key is pressed returning an empty string
                 case 3:
+                //Roll all the dice. Preliminary proof of concept
+                /*
                 Console.WriteLine("Rolling all the dice\n");
                     this.RollAllDice();
                     DisplayCurrentDiceValues();
                     if (IsYahtzee()) {
                         break;
                     }
+                */
+
+                //In this block I'm going to attempt to implement a way to keep specific die and reroll the others
+                //This this is actual way the game needs to be played
+                    
+                    this.PlayHand();
                     this.turnCounter++;
                     continue;
                 //Quit the game
